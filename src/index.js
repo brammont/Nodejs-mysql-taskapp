e0ef868de7a26b0db15c92ee01391bad6aff75d5
Brammont <brayanmazuera1@gmail.com>
2021-11-15 08:57:39 -0500

    configurando el motor del frontend html handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const Handlebars = require('express-handlebars');
 //incio del servidor
 const app= express();
 //consfigraciones
 app.set('port',process.env.PORT || 3000);
+app.engine('handlebars', Handlebars.engine({
+    defaultLayout: 'main',
+    //layoutsDir: path.join(app.get("views"), "layouts"),
+    //partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".handlebars",    
+ }));
 //middlewares
 app.use(morgan('dev'));
 //variables globales
