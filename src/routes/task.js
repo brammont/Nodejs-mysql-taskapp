8c882f8a5d99c1b73e8f39ef73260d586bbd7b26
Brammont <brayanmazuera1@gmail.com>
2021-11-15 14:17:59 -0500

    limpiando las rutas

--- src/routes/task.js
+++ src/routes/task.js
@@ -3,48 +3,48 @@ const router = express.Router();
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
-router.get('/crear', (req, res) => {
+router.get('/add', (req, res) => {
     res.render('tasks/add');
 });
 
 router.post('/add', async (req, res) => {
     const {title,url,description}= req.body;
-    const newLink = {
+    const newTask = {
         title,
         url,
         description,
     };
-    await pool.query('INSERT INTO links set ?', [newLink]); 
+    await pool.query('INSERT INTO tasks set ?', [newTask]); 
     res.redirect('/tasks');
 });
 router.get('/', isLoggedIn, async (req, res) => {
-    const tasks = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    const tasks = await pool.query('SELECT * FROM tasks WHERE user_id = ?', [req.user.id]);
     res.render('tasks/list', { tasks });
 });
 router.get('/delete/:id', async (req, res) => {
     const { id } = req.params;
-    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
-    req.flash('success', 'Link Removed Successfully');
-    res.redirect('/links');
+    await pool.query('DELETE FROM tasks WHERE ID = ?', [id]);
+    req.flash('success', 'Task Removed Successfully');
+    res.redirect('/tasks');
 });
 
 router.get('/edit/:id', async (req, res) => {
     const { id } = req.params;
-    const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
-    console.log(links);
+    const task = await pool.query('SELECT * FROM tasks WHERE id = ?', [id]);
+    console.log(task);
     res.render('tasks/edit', {link: links[0]});
 });
 
 router.post('/edit/:id', async (req, res) => {
     const { id } = req.params;
     const { title, description, url} = req.body; 
-    const newLink = {
+    const newTask = {
         title,
         description,
         url
     };
-    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
-    req.flash('success', 'Link Updated Successfully');
+    await pool.query('UPDATE links set ? WHERE id = ?', [newTask, id]);
+    req.flash('success', 'Task Updated Successfully');
     res.redirect('/tasks');
 });
 
