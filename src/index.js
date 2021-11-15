e600373474c36cc8751ccdf28d0af29340529ba1
Brammont <brayanmazuera1@gmail.com>
2021-11-15 09:05:28 -0500

    configurando el tipo de datos que recibe la app desde los formularios

--- src/index.js
+++ src/index.js
@@ -6,14 +6,19 @@ const Handlebars = require('express-handlebars');
 const app= express();
 //consfigraciones
 app.set('port',process.env.PORT || 3000);
+app.set('views', path.join(__dirname ,'views'));
 app.engine('handlebars', Handlebars.engine({
     defaultLayout: 'main',
-    //layoutsDir: path.join(app.get("views"), "layouts"),
-    //partialsDir: path.join(app.get("views"), "partials"),
-    extname: ".handlebars",    
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".handlebars",
+    helpers: require('./lib/handlebars')
  }));
+ app.set("view engine", "handlebars");
 //middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 //variables globales
 
 //
